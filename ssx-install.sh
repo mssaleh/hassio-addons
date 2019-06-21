6c668b1452e2e15f094bc0eb4fc863f9cfc61f0d
Mohammed Saleh <mssaleh@users.noreply.github.com>
2019-06-21 19:38:44 +0400

    Create ssx-install.sh

---
+++ ssx-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Please answer with Y or Yes to any prompts you may get."
+
+apt update && sudo apt upgrade -y
+apt remove --purge *4.15.0-1021* *4.15.0-1040* *lxd*
+update-grub && sudo update-grub2 && sudo update-initramfs -u
+apt install apparmor-utils apt-transport-https bash ca-certificates curl dbus git gnupg2 jq python3-pip libpython3-dev libpython-dev ncdu openssl perl python python3-dev software-properties-common zip gnupg-agent
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt-get install -y nodejs
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && sudo apt install docker-ce -y
+usermod -aG docker ubuntu
+apt-get clean && sudo apt autoremove --purge && sudo apt-get autoclean
+docker run -d -p 80:8388 -p 80:8388/udp --restart="unless-stopped" -e TIMEOUT=600 -e METHOD="aes-256-cfb" -e PASSWORD=YOUR_PASSWORD -e ARGS="--reuse-port" shadowsocks/shadowsocks-libev
