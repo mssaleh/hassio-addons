89edf8b61a97da122b88233de773b11c5d4f40fe
Mohammed Saleh <mssaleh@users.noreply.github.com>
2019-06-22 08:17:56 +0400

    Update ssx-install.sh

--- ssx-install.sh
+++ ssx-install.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
 set -e
+ARCH=$(uname -m)
+
+# Parse command line parameters
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+
+    case $arg in
+        -p|--port)
+            PORT=$2
+            shift
+            ;;
+        -k|--password)
+            PSWD=$2
+            shift
+            ;;
+        *)
+            echo "[Error] Unrecognized option $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Generate hardware options
+case $ARCH in
+    "x86_64")
+        MACHINE=amd64
+    ;;
+    "armv7l")
+        MACHINE=armhf
+    ;;
+    "aarch64")
+        MACHINE=arm64
+    *)
+        echo "[Error] $ARCH not supported!"
+        exit 1
+    ;;
+esac
+
+if [ -z $PORT ]; then
+    PORT=8388
+fi
+if [ -z $PSWD ]; then
+    PSWD=ShdwSoxPrxy
+fi
+
 
 echo "Welcome to this convenience script by Mohammed Saleh"
 echo "===================================================="
@@ -8,15 +54,7 @@ echo "It is not suitable for ARM systems such as Raspberry Pi."
 echo "The purpose of this script is to deploy a Shadowsocks proxy server, usually on a VPS such as Amazon Lightsail or DigitalOcean."
 echo "Please make sure to open the same port you choose for this server on both TCP and UDP protocols (from your instance/droplet/firewall settings), and also set an external STATIC IP for the server."
 echo "========================================================================="
-echo "Please choose a Port number for the proxy server (e.g. 80 or 8388, etc.)"
-read -p "Proxy Port: " PORT
-echo "Please choose a Password for the proxy server (e.g. 80 or 8388, etc.)"
-read -p "Proxy Port: " PSWD
-echo "==========================================================================="
-echo "Now you are ready to go!"
-echo "Please answer with Y or Yes to any prompts you may get during this process!"
-echo "==========================================================================="
-read -p "Press enter to continue"
+sleep 10
 
 apt update && sudo apt upgrade -y
 # apt remove --purge *4.15.0-1021* *4.15.0-1040* *lxd*
@@ -28,7 +66,7 @@ apt install apparmor-utils apt-transport-https bash ca-certificates curl dbus gi
 # echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 # sudo apt-get update && sudo apt-get install yarn -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=$MACHINE] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update && sudo apt install docker-ce -y
 usermod -aG docker ubuntu
 apt-get clean && sudo apt autoremove --purge && sudo apt-get autoclean
@@ -36,5 +74,6 @@ docker run -d -p $PORT:8388 -p $PORT:8388/udp --restart="unless-stopped" -e METH
 
 echo "==========================================================================="
 echo "Deployment completed. Please verify the result manually by trying to connect to the proxy using a Shadowsocks client."
-read -p "Press enter to continue"
+echo "Server will now reboot!"
+sleep 10
 reboot
